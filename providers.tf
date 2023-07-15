4359d143f85c46655d2da1e231d6458917ef3eb7
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-07-15 14:49:25 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,10 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.0.0"
+      version = "5.8.0"
     }
   }
-  required_version = ">= 1.1.0"
 
   cloud {
     organization = "cloudysky"
