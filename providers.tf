1995e93147aca7fdb765526354e63493b9e6a266
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2024-01-15 07:48:42 -0500

    add

--- providers.tf
+++ providers.tf
@@ -1,10 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.8.0"
-    }
-  }
 
   backend "remote" {
     hostname     = "app.terraform.io"
