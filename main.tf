5a1e3153c0668048def3d3206b4175627a4bbfd3
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2024-01-15 06:36:30 -0500

    add

--- main.tf
+++ main.tf
@@ -138,7 +138,7 @@ resource "aws_security_group" "cloudsky-sg-1" {
 
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "sleep 6000"
+    command = "sleep 500"
   }
 }
 
